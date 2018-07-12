749263d45cea8f16393d67e402c25a1bef3676f3
Jake Levine <jgl2832@gmail.com>
2018-07-12 11:16:29 +0200

    dependancy loading

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,55 +1,56 @@
 //TODO Add an element of pancake selling
 //TODO add things like syrup and berries to make pancakes sell for more $$
 
-$( document ).ready( function() {
-	$.when(
-		$.getScript("src/scripts/classes.js"),
-		$.getScript("src/scripts/resources.js")
-	).done(function() {
-		for (var upgrade of upgrades) {
-			$("#upgrades").append(upgrade.html);
-		}
-		for (var button of buttons) {
-			$("#buttons").append(button.html);
-		}
-		$("#resources").append(resources.html);
-		var update = function() {
-			// get current income persecond
-			var incomePerSec = resources.totalIncomePerSecond();
+var readyFunction = function() {
+	for (var upgrade of upgrades) {
+		$("#upgrades").append(upgrade.html);
+	}
+	for (var button of buttons) {
+		$("#buttons").append(button.html);
+	}
+	$("#resources").append(resources.html);
+	var update = function() {
+		// get current income persecond
+		var incomePerSec = resources.totalIncomePerSecond();
 
-			// Update HTML elements
-			for (var resource of resources.list) {
-				// Update income
-				var perSec = resource.name in incomePerSec ? incomePerSec[resource.name] : 0
-				var perFrame = perSec / fps;
-				resource.count += perFrame;
-				if ( perSec > 0 ) {
-					var incomeIdContainer = $("#"+resource.incomeIdContainer);
-					if (incomeIdContainer.is(":hidden")) {
-						incomeIdContainer.show();
-					}
-					$("#"+resource.incomeId).text(Math.floor(perSec) + "/sec");
-				} else {
-					$("#"+resource.incomeIdContainer).hide();
+		// Update HTML elements
+		for (var resource of resources.list) {
+			// Update income
+			var perSec = resource.name in incomePerSec ? incomePerSec[resource.name] : 0
+			var perFrame = perSec / fps;
+			resource.count += perFrame;
+			if ( perSec > 0 ) {
+				var incomeIdContainer = $("#"+resource.incomeIdContainer);
+				if (incomeIdContainer.is(":hidden")) {
+					incomeIdContainer.show();
 				}
-				
-				// Update Resource Amounts
-				if (resource.count > 0 && $("#"+resource.nameId).is(":hidden")) {
-					$("#"+resource.nameId).show();
-					$("#"+resource.countId).show();
-				}
-				$("#"+resource.countId).html(Math.floor(resource.count));
-
-				// Update Costs
-				if (resource.cost) {
-					$("#"+resource.costId).html(multCost(buyMult, resource));
-				}
-
-				if (resource.count > 50 && $("#upgrades-wrapper").is(":hidden")) {
-					$("#upgrades-wrapper").show();
-				}
+				$("#"+resource.incomeId).text(Math.floor(perSec) + "/sec");
+			} else {
+				$("#"+resource.incomeIdContainer).hide();
 			}
-		};
-		setInterval(update, 1000 / fps);
+			
+			// Update Resource Amounts
+			if (resource.count > 0 && $("#"+resource.nameId).is(":hidden")) {
+				$("#"+resource.nameId).show();
+				$("#"+resource.countId).show();
+			}
+			$("#"+resource.countId).html(Math.floor(resource.count));
+
+			// Update Costs
+			if (resource.cost) {
+				$("#"+resource.costId).html(multCost(buyMult, resource));
+			}
+
+			if (resource.count > 50 && $("#upgrades-wrapper").is(":hidden")) {
+				$("#upgrades-wrapper").show();
+			}
+		}
+	};
+	setInterval(update, 1000 / fps);
+}
+
+$( document ).ready( function() {
+	$.when($.getScript("src/scripts/classes.js")).done(function () {
+		$.when($.getScript("src/scripts/resources.js").done(readyFunction))
 	});
 });
